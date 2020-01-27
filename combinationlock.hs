ad66f33acd94cf63373afdad86a38014af724ccf
danielc777888 <danielc777888@gmail.com>
2020-01-27 22:29:31 +0200

    combinationlock:Accepted

--- combinationlock.hs
+++ combinationlock.hs
@@ -3,10 +3,17 @@ main :: IO()
 main = interact (writeOutput . solve . readInput)
 
 readInput :: String -> [[Int]]
-readInput = map (map read . words) .  lines  
+readInput = map (map read . words) . filter (/= "0 0 0 0") .  lines  
 
 solve :: [[Int]] -> [Int]
-solve = map sum
+solve = map solve'
+
+solve' :: [Int] -> Int
+solve' (s:n1:n2:n3:[]) = (step1 + step2 + step3) * 9
+  where step1 = 80 + (wrapAround s n1 - n1 + s)
+        step2 = 40 + (wrapAround n2 n1 - n1 + n2)
+        step3 = n2 + (wrapAround n2 n3 - n3)
+        wrapAround x y = if x < y then 40 else 0
 
 writeOutput :: [Int] -> String
 writeOutput = unlines . map show
