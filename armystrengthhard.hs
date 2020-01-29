1d8a1f914de4d6ea816e3a975e6e0bedde2c0867
Daniel Cabral <daniel2@jemstep.com>
2020-01-29 15:50:24 +0200

    armystrengthhard:Accepted: Use Text instead of String

--- armystrengthhard.hs
+++ armystrengthhard.hs
@@ -1,32 +1,35 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as TI
 
 type Strength = Int
 type MonsterArmy = [Strength]
 type Battle = (MonsterArmy, MonsterArmy)
 
 main :: IO()
-main = interact (writeOutput . solve . readInput)
+main = TI.interact (writeOutput . solve . readInput)
 
-readInput :: String -> [Battle]
-readInput s = createBattles (filter (/= "") ( tail (lines s )))
-  --[([1], [1]), ([1,3,2], [5,5])] --map (roll . map read . words) . filter (/= "") .  lines  . tails
+readInput :: T.Text -> [Battle]
+readInput s = createBattles (filter (/= "") ( tail (T.lines s )))
 
-createBattles :: [String] -> [Battle]
+createBattles :: [T.Text] -> [Battle]
 createBattles [] = []
 createBattles xs = [(army1, army2)] ++ createBattles (drop 3 xs)
   where armies = tail (take 3 xs)
-        army1 = map read (words (armies !! 0))
-        army2 = map read (words (armies !! 1))
+        army1 = map (\t -> read (T.unpack t))  (T.words (armies !! 0))
+        army2 = map (\t -> read (T.unpack t))  (T.words (armies !! 1))
 
-solve :: [Battle] -> [String]
+solve :: [Battle] -> [T.Text]
 solve = map solve'
 
-solve' :: (MonsterArmy, MonsterArmy) -> String
+solve' :: (MonsterArmy, MonsterArmy) -> T.Text
 solve' (m1, m2) = if maxM1 == maxM2 then "Godzilla"
                   else if maxM2 > maxM1 then "MechaGodzilla"
                   else "Godzilla"
   where maxM2 = maximum m2
         maxM1 = maximum m1
 
-writeOutput :: [String] -> String
-writeOutput = unlines
+writeOutput :: [T.Text] -> T.Text
+writeOutput = T.unlines
 
